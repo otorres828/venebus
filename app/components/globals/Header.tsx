fa9a418980173e11d3312c5701c665da2db4a133
Oliver English <olivertorres1997@gmail.com>
2026-01-25 20:08:32 -0600

    ajuste de wizar

--- app/components/globals/Header.tsx
+++ app/components/globals/Header.tsx
@@ -32,7 +32,7 @@ function WizardHeader() {
         </div>
       </header>
       {/* Menú móvil fijo abajo */}
-      <nav className="fixed bottom-0 left-0 right-0 z-40 bg-white dark:bg-background-dark border-t border-gray-100 dark:border-gray-700 flex justify-around items-center h-16 lg:hidden">
+      {/* <nav className="fixed bottom-0 left-0 right-0 z-40 bg-white dark:bg-background-dark border-t border-gray-100 dark:border-gray-700 flex justify-around items-center h-16 lg:hidden">
         <Link to="/" className="flex flex-col items-center text-xs font-medium text-gray-500 hover:text-primary">
           <span className="material-symbols-outlined text-2xl">home</span>
           Inicio
@@ -49,7 +49,7 @@ function WizardHeader() {
           <span className="material-symbols-outlined text-2xl">help</span>
           Ayuda
         </Link>
-      </nav>
+      </nav> */}
     </>
   );
 }
